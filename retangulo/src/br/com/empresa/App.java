ed560bfc249e18da5a2994fc621be07ba327d502
Alice Cavalini <alice.scavalini@gmail.com>
2021-02-01 11:23:56 -0300

    01-02-2021

--- retangulo/src/br/com/empresa/App.java
+++ retangulo/src/br/com/empresa/App.java
@@ -10,5 +10,26 @@ public class App {
 		FrameRetangulo tela = new FrameRetangulo();
 		tela.criarTela();
 		
+		// Scanner teclado = new Scanner(System.in);
+		// Retangulo r1 = new Retangulo();
+		
+		// System.out.print("Digite a altura:");
+		// r1.setAltura(teclado.nextDouble())
+		
+		// System.out.print("Digite a base:");
+		// r1.setAltura(teclado.nextDouble());
+		
+		// System.out.println(r1.calculaArea());
+		// System.out.println(r1.calculaPerimetro());
+		// System.out.println(r1.calculaArea());
+		// System.out.println(r1.calculaPerimetro());
+		
+		// Retangulo r2 = new Retangulo();
+		// r2.setAltura(120);
+		// r2.setBase(33);
+		
+		// System.out.println(r2.calculaArea());
+		// System.out.println(r2.calculaPerimetro());
+
 	 }
 }
